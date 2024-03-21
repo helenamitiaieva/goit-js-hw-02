830a1ca1e195b4f30b9110d53ae13b4322823d19
hellena0606 <h0636641547@gmail.com>
2024-03-22 00:32:45 +0200

    tasks

--- js/task-3.js
+++ js/task-3.js
@@ -7,18 +7,18 @@
 // Якщо знайдено заборонене слово (spam або sale), то функція повертає буль true
 // Якщо в рядку відсутні заборонені слова, функція повертає буль false
 
-// function checkForSpam(message) {
-//   const lowerMessage = message.toLowerCase();
-//   if (lowerMessage.includes("spam" || "sale")) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
-// console.log(checkForSpam("Latest technology news")); // false
-// console.log(checkForSpam("JavaScript weekly newsletter")); // false
-// console.log(checkForSpam("Get best sale offers now!")); // true
-// console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-// console.log(checkForSpam("Trust me, this is not a spam message")); // true
-// console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-// console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+function checkForSpam(message) {
+  const lowerMessage = message.toLowerCase();
+  if (lowerMessage.includes("spam" || "sale")) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
